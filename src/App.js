26442ff9e525bea1a0c82c10838dad8739e54fec
TabogaBr <brenda.taboga7@gmail.com>
2023-05-10 16:40:49 +0200

    Task 3 - Calendar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CustomerList from './components/CustomerList';
 import TrainingList from './components/TrainingList';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+import PersonalTrainer from './components/PersonalTrainer';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
         <Link to="/">Home</Link>{' '}
         <Link to="/customers">Customer List</Link>{' '}
         <Link to="/trainings">Training List</Link>{' '}
+        <Link to="/personaltrainer">Personal Trainer</Link>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/customers" element={<CustomerList />} />
           <Route path="/trainings" element={<TrainingList />} />
+          <Route path="/personaltrainer" element={<PersonalTrainer />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
